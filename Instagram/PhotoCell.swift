a98508a74fb44ffb9ee32081a434f3153cbb08e7
Katie Jiang <katiejiang@fb.com>
2017-06-28 14:04:43 -0700

    Profile view controller basic UI

---
+++ Instagram/PhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.swift
+//  Instagram
+//
+//  Created by Katie Jiang on 6/28/17.
+//  Copyright © 2017 Katie Jiang. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+}
--- Instagram/ProfileViewController.swift
+++ Instagram/ProfileViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 import Parse
 
 class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var bioLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Make profile pic circular
+        profileImageView.layer.borderWidth = 1
+        profileImageView.layer.masksToBounds = false
+        profileImageView.layer.borderColor = UIColor.lightGray.cgColor
+        profileImageView.layer.cornerRadius = profileImageView.frame.height/2
+        profileImageView.clipsToBounds = true
+        
+        // Set navigation controller title to current username
+        self.navigationController?.navigationBar.topItem?.title = PFUser.current()?.username
     }
 
     override func didReceiveMemoryWarning() {
