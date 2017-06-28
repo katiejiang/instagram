bd78127400abc7ec2076910b033f839fdcd3bcf9
Katie Jiang <katiejiang@fb.com>
2017-06-28 10:52:00 -0700

    New post saves to database

--- Instagram/NewPostViewController.swift
+++ Instagram/NewPostViewController.swift
@@ -14,9 +14,8 @@ class NewPostViewController: UIViewController, UIImagePickerControllerDelegate, 
     var image: UIImage?
     
     @IBOutlet weak var photoImageView: UIImageView!
-    @IBOutlet weak var hiddenButton: UIButton!
-    
     @IBOutlet weak var captionField: UITextField!
+    @IBOutlet weak var instructionsLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,11 +52,23 @@ class NewPostViewController: UIViewController, UIImagePickerControllerDelegate, 
         self.present(vc, animated: true, completion: nil)
     }
     
+    @IBAction func onShare(_ sender: Any) {
+        Post.postUserImage(image: photoImageView.image, withCaption: captionField.text) { (success: Bool, error: Error?) in
+            if success {
+                print("successfully posted!")
+            } else if let error = error {
+                print(String(describing: error.localizedDescription))
+            }
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         let image = info[UIImagePickerControllerEditedImage] as? UIImage
         if image != nil {
             photoImageView.image = image
             captionField.isHidden = false
+            instructionsLabel.isHidden = true
             dismiss(animated: true, completion: nil)
         }
     }
---
+++ Instagram/ProfileViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileViewController.swift
+//  Instagram
+//
+//  Created by Katie Jiang on 6/28/17.
+//  Copyright © 2017 Katie Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
