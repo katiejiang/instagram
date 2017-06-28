3f689487817ee533990a28bc7f592467748b5c67
Katie Jiang <katiejiang@fb.com>
2017-06-28 11:28:10 -0700

    Feed table view shows usernames and captions

--- Instagram/FeedViewController.swift
+++ Instagram/FeedViewController.swift
@@ -7,15 +7,39 @@
 //
 
 import UIKit
+import Parse
 
 class FeedViewController: UIViewController, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var posts : [PFObject] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         // Do any additional setup after loading the view.
+        
+        updatePosts()
+    }
+    
+    func updatePosts() {
+        // Construct query
+        let query = PFQuery(className: "Post")
+        query.order(byDescending: "createdAt")
+        query.limit = 20
+        query.includeKey("author")
+        
+        // Fetch data asynchronously
+        query.findObjectsInBackground { (posts: [PFObject]?,
+            error: Error?) in
+            if let posts = posts {
+                self.posts = posts
+                self.tableView.reloadData()
+            } else {
+                print(String(describing: error?.localizedDescription))
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,15 +49,17 @@ class FeedViewController: UIViewController, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        let post = posts[indexPath.row]
+        let author = post["author"] as? PFUser
         
-        cell.captionLabel.text = "This is an edgy caption"
-        cell.usernameLabel.text = "Katie Jiang"
+        cell.usernameLabel.text = author?.username
+        cell.captionLabel.text = post["caption"] as? String
         
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return posts.count
     }
     
 }
